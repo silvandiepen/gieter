36274c483c00c43abd01d642abef6662b4a67de4
Sil van Diepen <me@sil.mt>
2020-12-24 10:15:16 +0100

    more options in markdown

--- src/libs/markdown.ts
+++ src/libs/markdown.ts
@@ -1,11 +1,21 @@
-const MarkdownIt = require("markdown-it");
+const MarkdownIt = require("markdown-it")({
+  html: true,
+  linkify: true,
+  typographer: true,
+  breaks: true,
+});
+import emoji from "markdown-it-emoji";
 import prism from "markdown-it-prism";
+import anchor from "markdown-it-anchor";
+
 import { extractMeta, removeMeta } from "./markdown-meta";
 import { MarkdownData } from "../types";
 
 const md = new MarkdownIt();
 
 md.use(prism);
+md.use(emoji);
+md.use(anchor);
 
 export const toHtml = async (input: string): Promise<MarkdownData> => {
   const metaData = await extractMeta(input);
