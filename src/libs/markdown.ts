b0b1e75087ce039f00ecce44f410028dfb802a53
Sil van Diepen <me@sil.mt>
2021-02-06 22:07:21 +0100

    update tasks

--- src/libs/markdown.ts
+++ src/libs/markdown.ts
@@ -2,7 +2,7 @@ const MarkdownIt = require("markdown-it");
 import emoji from "markdown-it-emoji";
 import prism from "markdown-it-prism";
 import anchor from "markdown-it-anchor";
-import taskLists from "markdown-it-tasks";
+import tasks from "markdown-it-tasks";
 
 import { extractMeta, removeMeta } from "./markdown-meta";
 import { MarkdownData } from "../types";
@@ -17,7 +17,7 @@ const md = new MarkdownIt({
 md.use(prism);
 md.use(emoji);
 md.use(anchor);
-md.use(taskLists, { enabled: true, label: true, labelAfter: true });
+md.use(tasks, { enabled: true, label: true, labelAfter: true });
 
 export const toHtml = async (input: string): Promise<MarkdownData> => {
   const metaData = await extractMeta(input);
